e5d5644cc7f9749869de37c0d5b9dc5a9fc5c096
sseifert <sseifert@pro-vision.de>
2015-06-05 18:51:41 +0200

    Add Configurable Emulator Provider for Touch UI Responsive Mode.

---
+++ ui/granite/src/main/java/io/wcm/wcm/ui/granite/emulator/impl/EmulatorProviderImpl.java
@@ -0,0 +1,147 @@
+/* Copyright (c) pro!vision GmbH. All rights reserved. */
+package io.wcm.wcm.ui.granite.emulator.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.commons.osgi.PropertiesUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.wcm.api.NameConstants;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.emulator.Emulator;
+import com.day.cq.wcm.emulator.EmulatorGroup;
+import com.day.cq.wcm.emulator.EmulatorProvider;
+import com.day.cq.wcm.mobile.api.device.DeviceGroup;
+import com.day.cq.wcm.mobile.api.device.DeviceGroupList;
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Simple implementation of {@link EmulatorProvider} to activate AEM 6.1 responsive mode for this application.
+ * To work it needs a property "cq:deviceGroups[]" set to "/etc/mobile/groups/responsive" in the site root page.
+ */
+@Component(immediate = true, metatype = true,
+label = "wcm.io Emulator Provider",
+description = "Provides emulators based on device groups in the pages with a configurable set of templates.")
+@Service(EmulatorProvider.class)
+public class EmulatorProviderImpl implements EmulatorProvider {
+
+  @Property(cardinality = Integer.MAX_VALUE,
+      label = "Template Patterns",
+      description = "List of regular expressions to match template paths this emulator provider should apply to.")
+  static final String PROPERYT_TEMPLATE_PATH_PATTERNS = "templatePathPatterns";
+
+  private List<Pattern> templatePathPatterns;
+
+  private static final Logger log = LoggerFactory.getLogger(EmulatorProviderImpl.class);
+
+  @Activate
+  void activate(Map<String,Object> config) {
+    templatePathPatterns = new ArrayList<>();
+    String[] patterns = PropertiesUtil.toStringArray(config.get(PROPERYT_TEMPLATE_PATH_PATTERNS), new String[0]);
+    for (String pattern : patterns) {
+      try {
+        templatePathPatterns.add(Pattern.compile(pattern));
+      }
+      catch (PatternSyntaxException ex) {
+        log.warn("Ignoring invalid template path pattern: " + pattern, ex);
+      }
+    }
+  }
+
+  @Override
+  public boolean handles(Resource resource) {
+    // check if resource is a page, and if the page uses a configured template
+    Page page = resource.adaptTo(Page.class);
+    if (page != null) {
+      String templatePath = page.getProperties().get(NameConstants.PN_TEMPLATE, String.class);
+      if (StringUtils.isNotEmpty(templatePath)) {
+        for (Pattern pattern : templatePathPatterns) {
+          Matcher matcher = pattern.matcher(templatePath);
+          if (matcher.matches()) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public List<Emulator> getEmulators(Resource resource) {
+    // return all emulators from all groups
+    List<Emulator> emulators = new ArrayList<>();
+    for (EmulatorGroup group : getEmulatorGroups(resource)) {
+      emulators.addAll(group.getEmulators());
+    }
+    return emulators;
+  }
+
+  @Override
+  public List<EmulatorGroup> getEmulatorGroups(Resource resource) {
+    // convert device groups defined in page props to emulator groups
+    Page page = resource.adaptTo(Page.class);
+    if (page != null) {
+      DeviceGroupList deviceGroups = page.adaptTo(DeviceGroupList.class);
+      if (deviceGroups != null) {
+        List<EmulatorGroup> emulatorGroups = new ArrayList<>();
+        for (DeviceGroup deviceGroup : deviceGroups) {
+          emulatorGroups.add(new EmulatorGroupImpl(deviceGroup));
+        }
+        return emulatorGroups;
+      }
+    }
+    return ImmutableList.of();
+  }
+
+
+  /**
+   * Delegates all calls to the given DeviceGroup.
+   */
+  private final class EmulatorGroupImpl implements EmulatorGroup {
+
+    private final DeviceGroup deviceGroup;
+
+    public EmulatorGroupImpl(DeviceGroup deviceGroup) {
+      this.deviceGroup = deviceGroup;
+    }
+
+    @Override
+    public String getTitle() {
+      return deviceGroup.getTitle();
+    }
+
+    @Override
+    public String getDescription() {
+      return deviceGroup.getDescription();
+    }
+
+    @Override
+    public String getPath() {
+      return deviceGroup.getPath();
+    }
+
+    @Override
+    public String getName() {
+      return deviceGroup.getName();
+    }
+
+    @Override
+    public List<Emulator> getEmulators() {
+      return deviceGroup.getEmulators();
+    }
+
+  }
+
+}
---
+++ ui/granite/src/test/java/io/wcm/wcm/ui/granite/emulator/impl/EmulatorProviderImplTest.java
@@ -0,0 +1,109 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2015 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.wcm.ui.granite.emulator.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import io.wcm.sling.commons.resource.ImmutableValueMap;
+import io.wcm.testing.mock.aem.junit.AemContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.sling.api.resource.Resource;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.emulator.Emulator;
+import com.day.cq.wcm.emulator.EmulatorProvider;
+import com.day.cq.wcm.mobile.api.device.DeviceGroup;
+import com.day.cq.wcm.mobile.api.device.DeviceGroupList;
+import com.google.common.collect.ImmutableList;
+
+public class EmulatorProviderImplTest {
+
+  @Rule
+  public AemContext context = new AemContext();
+
+  private EmulatorProvider underTest;
+
+  private Page page1;
+  private Page page2;
+  private Page page3;
+
+  @Before
+  public void setUp() {
+    underTest = context.registerInjectActivateService(new EmulatorProviderImpl(), ImmutableValueMap.builder()
+        .put(EmulatorProviderImpl.PROPERYT_TEMPLATE_PATH_PATTERNS, new String[] {
+            "^/apps/app1/.*$",
+            "^/apps/app2/.*$"
+        })
+        .build());
+
+    page1 = context.create().page("/content/page1", "/apps/app1/template1",
+        ImmutableValueMap.of("cq:deviceGroups", new String[] {
+            "/etc/mobile/groups/responsive"
+        }));
+    page2 = context.create().page("/content/page1/page2", "/apps/app2/template2");
+    page3 = context.create().page("/content/page1/page3", "/apps/app3/template3");
+  }
+
+  @Test
+  public void testHandles() {
+    assertTrue(underTest.handles(page1.adaptTo(Resource.class)));
+    assertTrue(underTest.handles(page2.adaptTo(Resource.class)));
+    assertFalse(underTest.handles(page3.adaptTo(Resource.class)));
+  }
+
+  @Test
+  public void testGetEmulators() {
+    Resource resource = mock(Resource.class);
+    Page page = mock(Page.class);
+    when(resource.adaptTo(Page.class)).thenReturn(page);
+    DeviceGroupListMock deviceGroupList = new DeviceGroupListMock();
+    when(page.adaptTo(DeviceGroupList.class)).thenReturn(deviceGroupList);
+    DeviceGroup deviceGroup1 = mock(DeviceGroup.class);
+    Emulator emulator1 = mock(Emulator.class);
+    Emulator emulator2 = mock(Emulator.class);
+    deviceGroupList.add(deviceGroup1);
+    when(deviceGroup1.getEmulators()).thenReturn(ImmutableList.of(emulator1, emulator2));
+    DeviceGroup deviceGroup2 = mock(DeviceGroup.class);
+    Emulator emulator3 = mock(Emulator.class);
+    when(deviceGroup2.getEmulators()).thenReturn(ImmutableList.of(emulator3));
+    deviceGroupList.add(deviceGroup2);
+
+    List<Emulator> emulators = underTest.getEmulators(resource);
+    assertEquals(3, emulators.size());
+    assertTrue(emulators.contains(emulator1));
+    assertTrue(emulators.contains(emulator2));
+    assertTrue(emulators.contains(emulator3));
+  }
+
+  private static class DeviceGroupListMock extends ArrayList<DeviceGroup> implements DeviceGroupList {
+    private static final long serialVersionUID = 1L;
+    // DeviceGroup list
+  }
+
+}
