0f383adc37bf46bf074a8137c1c7db771e3347e6
sseifert <sseifert@pro-vision.de>
2016-01-26 15:23:20 +0100

    reduce visibility for sling model activate methods

--- commons/src/main/java/io/wcm/wcm/commons/ui/DenyWcmDisabled.java
+++ commons/src/main/java/io/wcm/wcm/commons/ui/DenyWcmDisabled.java
@@ -45,7 +45,7 @@ public class DenyWcmDisabled {
   private WCMMode wcmMode;
 
   @PostConstruct
-  protected void activate() throws IOException {
+  private void activate() throws IOException {
     if (wcmMode == WCMMode.DISABLED) {
       response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
